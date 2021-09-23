bd1b8bd3fa645b8f48fc8db4912f5fb7d41c8114
tsuzumi-sano <sn.mrd04@gmail.com>
2021-09-23 16:42:09 +0900

    style調整

--- projects.php
+++ projects.php
@@ -29,11 +29,14 @@
                             <img src="img/line.png" alt="装飾">
                             <p class="description">
                                 Twitterの複製アプリです。ツイートを投稿できます。<br>
-                                PHP / MySQLを利用しています。MVCモデルを用いて制作しました。<br>
-                                アカウント作成、画像のアップロード機能などの習得のため制作しました。<br>
+                                MVCモデルでの制作、アカウント作成、画像のアップロード機能などの習得のため制作しました。<br><br>
+                                使用言語、ツールなど<br>
+                                HTML / CSS / PHP / JavaScript / MySQL / Bootstrap / さくらVPS / GitHub / VScode / MySQL Workbench<br><br>
+                                テストアカウント<br>
+                                Email：test0001@example.com<br>
+                                Pass：test0001
+
                             </p>
-                            <p class="description-account">テストアカウント</p>
-                            <b>Email：test0001@example.com<br>Pass：test0001</b>
                             <div class="link-btn">
                                 <a href="https://test-twitterclone-ts.com/TwitterClone" target="_blank">Website</a>
                             </div>
@@ -47,11 +50,14 @@
                             <h3>ひとこと掲示板</h3>
                             <img src="img/line.png" alt="装飾">
                             <p class="description">
-                                シンプルな掲示板です。PHP / MySQLを利用しています。<br>
+                                シンプルな掲示板です。<br>
                                 管理画面から、CSVのダウンロードや投稿の編集・削除が可能です。<br>
-                                基本的なデータベース操作、セッション機能やバリデーション機能の実装を習得するため制作しました。
+                                基本的なデータベース操作、セッション機能やバリデーション機能の実装を習得するため制作しました。<br><br>
+                                使用言語、ツールなど<br>
+                                HTML / CSS / PHP / JavaScript / MySQL / さくらVPS / GitHub / VScode / MySQL Workbench<br><br>
+                                管理画面パスワード：adminPassword
                             </p>
-                            <p class="description-account">管理画面パスワード：adminPassword</p>
+                            
                             <div class="link-btn">
                                 <a href="https://simple-bbs-ts.net/BBS/base.php" target="_blank">Website</a>
                                 <a href="https://simple-bbs-ts.net/BBS/admin.php" target="_blank">管理画面</a>
